45a5300d080f8a56d12580fcb4728111fb8e0c63
liuyujia <417378336@qq.com>
2018-12-18 20:18:01 +0800

    first commit

--- 查看审批流程demo/ViewController.m
+++ 查看审批流程demo/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "constant.h"
+#import "FunctionDefine.h"
 
+@interface ViewController ()
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) UIDatePicker *startDatePicker;
 @end
 
 @implementation ViewController
@@ -17,7 +21,105 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
+
+
+- (IBAction)oneAction:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//    [defaults setInteger:0 forKey:@"HOST"];
+    
+    
+    UDSaveData(0, @"HOST");
+    [defaults synchronize];
+    
+//    NSLog(@"%@",Host);
+    
+}
+
+
+
+
+
+
+- (IBAction)twoAction:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:0 forKey:@"HOST"];
+    [defaults synchronize];
+    
+//    NSLog(@"%@",Host);
+}
+
+
+- (IBAction)threeAction:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:0 forKey:@"HOST"];
+    [defaults synchronize];
+    
+//    NSLog(@"%@",Host);
+}
+
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    static int i = 0;
+    i++;
+    NSLog(@"%d",i);
+}
+
+-(void)setUIWebviewcookie {
+    
+    NSString *token = @"eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDM4OTU2NDUsInN1YiI6IntcImhyT3JnYW5pemF0aW9uSWRcIjoxNjYzLFwibW9iaWxlXCI6XCIxODgyMDIwNjY2N1wiLFwidXNlckxvZ2luTmFtZVwiOlwiMjAxNzUyMThcIixcInVzZXJOYW1lXCI6XCLkv57liJrpkohcIixcInVzZXJJZFwiOjg2Nzl9IiwiZXhwIjoxNTQ0NTAwNDQ1LCJuYmYiOjE1NDM4OTU2NDV9.OqWklywmZojGShXSzLD7DvbomwJAuhk5zKAZgbnQH9I";
+    
+    NSDictionary *dic1 = [NSDictionary dictionaryWithObjectsAndKeys:
+                         @"zhengtongautoSSOCookieV1", NSHTTPCookieName,
+                         token, NSHTTPCookieValue,
+                         @".zhengtongauto.com", NSHTTPCookieDomain,//[url host], NSHTTPCookieDomain,//
+                         @"/", NSHTTPCookiePath,//[url path], NSHTTPCookiePath,//
+                         @"7200", NSHTTPCookieMaximumAge,
+                         nil];
+    
+    NSHTTPCookie *cookie1 = [NSHTTPCookie cookieWithProperties:dic1];
+    
+    NSDictionary *dic2 = [NSDictionary dictionaryWithObjectsAndKeys:
+                          @"domain", NSHTTPCookieName,
+                          @".zhengtongauto.com", NSHTTPCookieValue,
+                          @".zhengtongauto.com", NSHTTPCookieDomain,//[url host], NSHTTPCookieDomain,//
+                          @"/", NSHTTPCookiePath,//[url path], NSHTTPCookiePath,//
+                          @"7200", NSHTTPCookieMaximumAge,
+                         nil];
+    NSHTTPCookie *cookie2 = [NSHTTPCookie cookieWithProperties:dic2];
+
+    NSDictionary *dic3 = [NSDictionary dictionaryWithObjectsAndKeys:
+                          @"path", NSHTTPCookieName,
+                          @"/", NSHTTPCookieValue,
+                          @".zhengtongauto.com", NSHTTPCookieDomain,//[url host], NSHTTPCookieDomain,//
+                          @"/", NSHTTPCookiePath,//[url path], NSHTTPCookiePath,//
+                          @"7200", NSHTTPCookieMaximumAge,
+                          nil];
+    NSHTTPCookie *cookie3 = [NSHTTPCookie cookieWithProperties:dic3];
+
+    NSDictionary *dic4 = [NSDictionary dictionaryWithObjectsAndKeys:
+                          @"max-age", NSHTTPCookieName,
+                          @"7200", NSHTTPCookieValue,
+                          @".zhengtongauto.com", NSHTTPCookieDomain,//[url host], NSHTTPCookieDomain,//
+                          @"/", NSHTTPCookiePath,//[url path], NSHTTPCookiePath,//
+                          @"7200", NSHTTPCookieMaximumAge,
+                          nil];
+    NSHTTPCookie *cookie4 = [NSHTTPCookie cookieWithProperties:dic4];
+
+    
+    
+    NSURL *url = [NSURL URLWithString:@"http://oadev.zhengtongauto.com:8010/Portal/InstanceSheetsSimple.html?IsMobile=true&InstanceId=d8ceb95a-20c6-402e-a91f-3f416d77a18f"];
+    NSMutableURLRequest *request= [NSMutableURLRequest requestWithURL:url];
+    NSArray *arrCookies = [NSArray arrayWithObjects:cookie1,cookie4,cookie2,cookie3, nil];
+    NSDictionary *dictCookies = [NSHTTPCookie requestHeaderFieldsWithCookies:arrCookies];
+    [request setValue:[dictCookies objectForKey:@"Cookie"] forHTTPHeaderField:@"Cookie"];
+    
+    [self.webView loadRequest:request];
+}
+
 
 
 @end
--- 查看审批流程demo/constant.h
+++ 查看审批流程demo/constant.h
@@ -10,4 +10,55 @@
 #define constant_h
 
 
+//#define UDTakeData(key) 1
+
+
+//#ifndef ISTORE_ENVIRONMENT
+//
+//#define ISTORE_ENVIRONMENT UDTakeData(@"HOST")
+//
+//
+//#if ISTORE_ENVIRONMENT == 0
+//
+////开发环境
+//#define Host @"http://10.10.20.168:8081/"
+//
+//#elif ISTORE_ENVIRONMENT == 1
+////正式环境
+//#define Host @"http://app.istore8.cn/"
+//
+//#elif ISTORE_ENVIRONMENT == 2
+////测试环境
+//#define Host @"https://app-test.istore8.cn/"
+
+
+
+
+
+
+
+
+
+//#endif
+//#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif /* constant_h */
